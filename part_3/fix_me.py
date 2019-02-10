1aa5119fe033e050fd09ca640267c6dd07323977
Taylor <tep0007@auburn.edu>
2019-02-10 17:57:19 -0600

    set a and b as integers

--- part_3/fix_me.py
+++ part_3/fix_me.py
@@ -14,6 +14,8 @@ print("You entered", b, "which is a", type(b))
 # HINT: why would we be checking what type a and b are again?
 #       Let's assume we want a and b to be integers at this point
 #######################################################################
+a=int(a)
+b=int(b)
 
 print("What is", a, "now?", type(a))
 print("What is", b, "now?", type(a))
